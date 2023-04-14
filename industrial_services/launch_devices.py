407f1079dce9c07e2453f5737bffce41f2f86c3a
flavia <flavia.monti95@gmail.com>
2023-04-14 14:24:58 +0200

    [fix-add] script client e descriptions

--- industrial_services/launch_devices.py
+++ industrial_services/launch_devices.py
@@ -7,13 +7,19 @@ import json
 
 config_json = json.load(open('../config.json', 'r'))
 mode = config_json['mode']
+phase = config_json['phase']
+size = config_json['size']
 
 
 if __name__ == "__main__":
 
     processes: List[subprocess.Popen] = []
     
-    list_devices = glob.glob(f"actors_api_{mode}/descriptions/*.json")
+    if phase == 1:
+        list_devices = glob.glob(f"actors_api_{mode}/descriptions_phase{phase}/*.json")
+    elif phase == 2:
+        list_devices = glob.glob(f"actors_api_{mode}/descriptions_phase{phase}/{size}/*.json")
+
     for configuration_path in list_devices:
         script = f"run_target_{mode}.py" if "target" in configuration_path else f"run_service_{mode}.py"
         process = subprocess.Popen([sys.executable, script, configuration_path])
