4a5589b7be02d2c3f5507100d4261dd028656fe1
flavia <flavia.monti95@gmail.com>
2023-11-04 09:03:27 +0100

    fix bug

--- industrial_services/launch_devices.py
+++ industrial_services/launch_devices.py
@@ -4,24 +4,14 @@ import glob
 from typing import List
 import json
 
-
-config_json = json.load(open('../config.json', 'r'))
-mode = config_json['mode']
-phase = config_json['phase']
-size = config_json['size']
-
-
 if __name__ == "__main__":
 
     processes: List[subprocess.Popen] = []
     
-    if phase == 1:
-        list_devices = glob.glob(f"actors_api_{mode}/descriptions_phase{phase}/*.json")
-    elif phase == 2:
-        list_devices = glob.glob(f"actors_api_{mode}/descriptions_phase{phase}/{size}/*.json")
-
+    list_devices = glob.glob(f"actors_api_plan/descriptions/*.json")
+    
     for configuration_path in list_devices:
-        script = f"run_target_{mode}.py" if "target" in configuration_path else f"run_service_{mode}.py"
+        script = f"run_service_plan.py"
         process = subprocess.Popen([sys.executable, script, configuration_path])
         processes.append(process)
 
